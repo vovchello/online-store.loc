81b928e26061efa40730790ef4816aadff2b772a
vovchello <vovka9379@gmail.com>
2018-10-03 20:18:10 +0300

    kill it

---
+++ app/Http/Controllers/Front/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: php
+ * Date: 03.10.18
+ * Time: 19:48
+ */
+
+namespace App\Http\Controllers\Front;
+
+
+class HomeController
+{
+
+}
---
+++ config/dammydata.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: php
+ * Date: 03.10.18
+ * Time: 19:29
+ */
