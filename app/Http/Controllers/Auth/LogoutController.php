182f38a9c4e65f04c231bca4f70f6b4a28a7bf7b
vovchello <vovka9379@gmail.com>
2018-10-23 13:21:58 +0300

    add auth

---
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: panda
+ * Date: 23.10.18
+ * Time: 11:45
+ */
+
+namespace App\Http\Controllers\Auth;
+
+
+class LogoutController
+{
+
+}
---
+++ app/User.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: panda
+ * Date: 23.10.18
+ * Time: 10:50
+ */
+
+namespace App;
+
+
+class User
+{
+
+}
